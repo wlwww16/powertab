2de7204d52d85044260e97a7d3b8361293b9595a
wlw-codes <will.lewis.w@gmail.com>
2022-06-06 21:09:21 +0100

    file reorder ahead of theme support

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const search = document.querySelector('#searchInput')
+
+				//search.focus();
+
+				document.addEventListener('keydown', function(e) {
+              				//if key pressed is Enter key
+              				if (e.keyCode ===  13) {
+						e.preventDefault();
+
+						if (isValidURL(search.value)) {
+							window.location.assign("https://" + search.value);
+							return;
+						}
+
+						window.location.assign("https://duckduckgo.com/?q=" + search.value);
+					}
+				})
+			});
+
+			function isValidURL(string) {
+				var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+
+				return (res !== null);
+			};
